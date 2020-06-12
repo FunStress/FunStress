e326d90a14f8e4e9ee62b420d34d03f737e6b184
Vamsikvkr <vamsikvkr@Vamsikvkrs-MacBook-Pro.local>
2020-06-12 08:40:26 -0700

    letter images to contacts

--- StressBuster/UI Elements/DesignableTextView.swift
+++ StressBuster/UI Elements/DesignableTextView.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable
+class DesignableTextView: UITextView {
+    
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+}
---
+++ StressBuster/View Controllers/Shared/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extensions.swift
+//  StressBuster
+//
+//  Created by Vamsikvkr on 6/12/20.
+//  Copyright © 2020 StressBuster. All rights reserved.
+//
+
+import Foundation
